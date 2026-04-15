f83b8625928e9e48babce92d9a5f4c809cbf24a2
KatanaMaster <eg.gasilin2018@yandex.ru>
2026-04-15 20:46:44 +0300

    history add

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import Header from './Components/Header.vue'
 import Form from './Components/Form.vue';
+import History from './Components/History.vue';
 </script>
 <template>
   <div class="app">
     <Header />
     <div class="form">
       <Form />
+    </div>
+    <div class="history">
+      <History />
     </div>
   </div>
 </template>
@@ -17,5 +21,15 @@ import Form from './Components/Form.vue';
   padding: 0;
   box-sizing: border-box;
   background-color: #5f5f5f;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.history {
+  margin: 20px;
 }
 </style>
